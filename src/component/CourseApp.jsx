8a09a898cc0bcb9c5250c834a906839a653f9e99
Maria Mendeleva <mmendeleva@griddynamics.com>
2021-04-14 18:24:23 +0300

    Added blank ListUsersComponent

--- src/component/CourseApp.jsx
+++ src/component/CourseApp.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import ListUsersComponent from './ListUsersComponent';
 
 class CourseApp extends Component {
     render() {
         return (
+            <>
               <h1>Courses Application</h1>
+              <ListUsersComponent/>
+            </>  
         )
     }
 }
