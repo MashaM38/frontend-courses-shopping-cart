35aca30dbc023de22cd1afcf05e50768515d6fc6
Maria Mendeleva <mmendeleva@griddynamics.com>
2021-04-14 17:42:36 +0300

    Adjusted empty React project

---
+++ src/component/CourseApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CourseApp extends Component {
+    render() {
+        return (
+              <h1>Courses Application</h1>
+        )
+    }
+}
+
+export default CourseApp
